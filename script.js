f86d63ec0ade85b2372aa4869fee528ffb143669
PorousBunion8 <126472498+PorousBunion8@users.noreply.github.com>
2023-05-15 08:34:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function convertToFahrenheit() {
+    var fahrenheitInput = document.getElementById("fahrenheitInput");
+    var resultElement = document.getElementById("result");
+  
+    if (fahrenheitInput.value === "" || isNaN(fahrenheitInput.value)) {
+      resultElement.innerText = "Por favor, insira um valor válido em Fahrenheit.";
+      return;
+    }
+  
+    var fahrenheit = parseFloat(fahrenheitInput.value);
+    var celsius = (fahrenheit - 32) * 5 / 9;
+  
+    resultElement.innerText = fahrenheit + " Fahrenheit é igual a " + celsius.toFixed(2) + " Celsius.";
+  
+    
+    var body = document.body;
+    if (celsius > 30) {
+      body.style.backgroundColor = "#ffa500"; 
+    } else {
+      body.style.backgroundColor = "#87ceeb"; 
+    }
+  }
+  
+  
+  
+  
+  
+  
